cc23d1bf7abf0ab583152d5e029b72afb1336fee
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-20 23:04:55 +0600

    start project

---
+++ commands/jarvis.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+import webbrowser
+import speech_recognition
+import pyttsx3, os
+from pydub import AudioSegment
+from pydub.playback import play
+
+command = {
+    'se': {
+        'jarvis': ('джарвис', 'привет джарвис', 'jarvis'),
+        'break': ('стоп', 'break'),
+        'thanks': ('спасибо', 'красавчик', 'благодарю'),
+        'negative': ('да не ты тупой', 'ты тупой', 'не ты тупой')
+    }
+}
+
+sr = speech_recognition.Recognizer()
+sr.pause_threshold = 0.5
+speak_engine = pyttsx3.init()
+
+def speak(what):
+    speak_engine.say(what)
+    speak_engine.runAndWait()
+    speak_engine.stop()
