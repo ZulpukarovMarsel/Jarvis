5aee44659e0b5c5976aa8b0b3d9cac07f871f220
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-20 23:09:52 +0600

    added function(activate_jarvis, process_command)

--- commands/jarvis.py
+++ commands/jarvis.py
@@ -43,3 +43,28 @@ def my_function():
 def play_audio(filename):
     sound = AudioSegment.from_file(filename, format="wav")
     play(sound)
+
+
+def activate_jarvis():
+    play_audio('sounds/app_sound_jarvis-og_greet1.wav')
+
+
+def process_command(cmd):
+    if any(word in cmd for word in command['cmds']['audio']):
+        play_audio('sounds/app_sound_jarvis-og_ok2.wav')
+        webbrowser.open_new_tab('https://www.youtube.com/watch?v=fjUGC8g4GOE')
+    elif any(word in cmd for word in command['cmds']['youtube']):
+        play_audio('sounds/app_sound_jarvis-og_ok2.wav')
+        webbrowser.open_new_tab('https://www.youtube.com/')
+    elif any(word in cmd for word in command['cmds']['kino']):
+        play_audio('sounds/app_sound_jarvis-og_ok3.wav')
+        webbrowser.open_new_tab('https://kinojump.com/')
+    elif any(word in cmd for word in command['cmds']['github']):
+        play_audio('sounds/app_sound_jarvis-og_ok4.wav')
+        webbrowser.open_new_tab('https://github.com/MarsGeeks/')
+    elif any(word in cmd for word in command['cmds']['time']):
+        speak(time())
+    elif any(word == cmd for word in command['os']['switch_off']):
+        play_audio('sounds/app_sound_jarvis-og_off.wav')
+        os.system("shutdown /s /t 0")
+
