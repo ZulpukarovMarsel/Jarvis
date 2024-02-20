8e0b29e732fe04605be604bb0b9479c7ad6b6095
ZulpukarovMarsel <zulpukarovmarsel17@gmail.com>
2024-02-20 23:12:49 +0600

    added position

---
+++ commands/position.py
@@ -0,0 +1,14 @@
+import os, random, time, pyautogui
+
+pyautogui.PAUSE = 0.2
+pyautogui.FAILSAFE = True
+
+try:
+    while True:
+        x, y = pyautogui.position()
+        positionStr = 'X: ' + str(x).rjust(4) + ' Y: ' + str(y).rjust(4)
+        print(positionStr, end='')
+        print('\b' * len(positionStr), end='', flush=True)
+
+except:
+    os.system('clear')
